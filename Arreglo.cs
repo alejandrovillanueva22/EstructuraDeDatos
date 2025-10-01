194f854f7a5259b8e9d0bfce14debab7420fae62
alejandrovillanueva22 <240173@upbc.edu.mx>
2025-09-30 17:42:05 -0700

    Add files via upload

---
+++ Arreglo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class ArregloGen<T> where T : IComparable<T>
+{
+    private T[] elementos;
+    private int contador;
+
+    public ArregloGen(int capacidad)
+    {
+        elementos = new T[capacidad];
+        contador = 0;
+    }
+
+    public void Agregar(T elemento)
+    {
+        if (contador < elementos.Length)
+        {
+            elementos[contador++] = elemento;
+        }
+        else
+        {
+            Console.WriteLine("Arreglo completo, no se pueden agregar mas elementos");
+        }
+    }
+
+    public T Obtener(int indice)
+    {
+        if (indice >= 0 && indice < contador)
+            return elementos[indice];
+        else
+            throw new IndexOutOfRangeException("Índice fuera de rango.");
+    }
+
+    public void Eliminar(int indice)
+    {
+        if (indice >= 0 && indice < contador)
+        {
+            for (int i = indice; i < contador - 1; i++)
+            {
+                elementos[i] = elementos[i + 1];
+            }
+            contador--;
+        }
+        else
+        {
+            Console.WriteLine("Índice inválido.");
+        }
+    }
+
+    public void Mostrar()
+    {
+        for (int i = 0; i < contador; i++)
+        {
+            Console.Write(elementos[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void Ordenar()
+    {
+        // Bubble Sort
+        for (int i = 0; i < contador - 1; i++)
+        {
+            for (int j = 0; j < contador - i - 1; j++)
+            {
+                if (elementos[j].CompareTo(elementos[j + 1]) > 0)
+                {
+                    // intercambiar
+                    T temp = elementos[j];
+                    elementos[j] = elementos[j + 1];
+                    elementos[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿
+// Programa Principal
+class Program
+{
+    static void Main()
+    {
+        int opcion;
+        do
+        {
+            Console.WriteLine("\n==== MENÚ ====");
+            Console.WriteLine("1. Demostración con enteros");
+            Console.WriteLine("2. Demostración con cadenas");
+            Console.WriteLine("3. Demostración con alumnos");
+            Console.WriteLine("4. Salir");
+            Console.Write("Elige una opción: ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    DEnteros();
+                    break;
+                case 2:
+                    DCadenas();
+                    break;
+                case 3:
+                    DAlumnos();
+                    break;
+            }
+        } while (opcion != 4);
+    }
+
+    static void DEnteros()
+    {
+        var numeros = new ArregloGen<int>(6);
+        numeros.Agregar(110);
+        numeros.Agregar(22);
+        numeros.Agregar(87);
+        numeros.Agregar(77);
+        numeros.Agregar(7);
+        numeros.Agregar(26);
+
+
+        Console.WriteLine("\nAntes de ordenar:");
+        numeros.Mostrar();
+
+        numeros.Ordenar();
+        Console.WriteLine("Después de ordenar:");
+        numeros.Mostrar();
+
+    }
+
+    static void DCadenas()
+    {
+        var palabras = new ArregloGen<string>(6);
+        palabras.Agregar("Cafe");
+        palabras.Agregar("Agua");
+        palabras.Agregar("Mariposa");
+        palabras.Agregar("Pozole");
+        palabras.Agregar("Pizza");
+        palabras.Agregar("Periodico");
+
+        Console.WriteLine("\nAntes de ordenar:");
+        palabras.Mostrar();
+
+        palabras.Ordenar();
+        Console.WriteLine("Después de ordenar:");
+        palabras.Mostrar();
+    }
+
+    static void DAlumnos()
+    {
+        var alumnos = new ArregloGen<Alumno>(6);
+        alumnos.Agregar(new Alumno("Alejando", 85));
+        alumnos.Agregar(new Alumno("Erika", 80));
+        alumnos.Agregar(new Alumno("Diego", 100));
+        alumnos.Agregar(new Alumno("Christian", 100));
+        alumnos.Agregar(new Alumno("Gerardo", 90));
+        alumnos.Agregar(new Alumno("Elliot", 85));
+
+
+        Console.WriteLine("\nAntes de ordenar:");
+        alumnos.Mostrar();
+
+        alumnos.Ordenar();
+        Console.WriteLine("Después de ordenar:");
+        alumnos.Mostrar();
+    }
+}
